398fcf491cd1e4a4bd0e7e8d68d902708d17f322
camposfabio987 <campos.fabio987@gmail.com>
2022-08-16 16:51:10 -0300

    todos os arquivos da biblioteca, construtores feito

---
+++ Biblioteca_de_Jogos/Classes/Jogo.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Biblioteca_de_Jogos
+{
+    internal class Jogo
+    {
+        // ATRIBUTOS
+
+        String titulo;
+        String genero;
+        int ano;
+        int maxJogadores;
+        int metacritic;
+
+        // CONSTRUTORES
+
+        public Jogo (String titulo, int ano, String genero, int maxJogadores, int metacritic)
+        {
+            this.titulo = titulo;
+            this.ano = ano;
+            this.genero = genero;
+            this.maxJogadores = maxJogadores;
+            this.metacritic = metacritic; 
+        }
+
+        public Jogo(String titulo, int ano, String genero, int maxJogadores)
+        {
+            this.titulo = titulo;
+            this.ano = ano;
+            this.genero = genero;
+            this.maxJogadores = maxJogadores;
+        }
+
+        // MÉTODOS DE ACESSO
+        // GETTER - RETORNA VALOR DO ATRIBUTO
+
+        public String getTitulo()
+        {
+            return this.titulo; 
+        }
+
+        // SETTER - MODIFICA VALOR DO ATRIBUTO
+
+        public void setTitulo(String titulo)
+        {
+            if(titulo != "")
+            {
+                this.titulo = titulo;
+            }
+        }
+
+        public int getAno()
+        {
+            return this.ano;
+        }
+
+
+
+
+
+    }
+}
---
+++ Biblioteca_de_Jogos/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Biblioteca_de_Jogos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Jogo> listaDeJogos = new List<Jogo>();
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            while (true)
+            {
+                Console.WriteLine("=====BIBLIOTECA DE JOGOS=====");
+
+                Console.WriteLine("1 - Adicionar Jogo");
+                Console.WriteLine("2 - Listar Jogos");
+                Console.WriteLine("3 - Editar Jogo");
+                Console.WriteLine("4 - Remover Jogo");
+                Console.Write("Opção: ");
+
+                bool resultado;
+                String opcao = Console.ReadLine();
+
+                switch(opcao)
+                {
+                    case "1":
+                        resultado = AdicionarJogo(listaDeJogos);
+                        if(resultado == true)
+                        {
+                            Console.WriteLine("Cadastrado com sucesso!");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Erro no cadastro.");
+                        }
+                        break;
+
+                    
+
+                }
+            }
+        }
+    }
+}
